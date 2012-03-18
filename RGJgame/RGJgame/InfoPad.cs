611d36a92e819a8acc828a276164c2989df93721
Tim Adam <timotheyadam@gmail.com>
2012-03-18 13:26:54 -0700

    Infopad update

--- RGJgame/RGJgame/InfoPad.cs
+++ RGJgame/RGJgame/InfoPad.cs
@@ -16,7 +16,12 @@ namespace RGJgame
 {
     class InfoPad : Entity
     {
+        public const int ANIMTIME = 100; 
+
         public String power;
+        Texture2D[] anim;
+        int totaltime;
+
 
         public InfoPad(Vector2 pos, String power)
             : base(pos)
@@ -27,17 +32,25 @@ namespace RGJgame
 
         public override void LoadContent(Game game)
         {
+            anim = new Texture2D[4];
+            anim[0] = game.Content.Load<Texture2D>(@"images/infopad1");
+            anim[1] = game.Content.Load<Texture2D>(@"images/infopad2");
+            anim[2] = game.Content.Load<Texture2D>(@"images/infopad3");
+            anim[3] = game.Content.Load<Texture2D>(@"images/infopad2");
+
             texture = game.Content.Load<Texture2D>(@"images/infoPanel");
         }
 
         public override void Update(GameTime gameTime)
         {
             Collisions.check(this, GameState.player);
+
+            totaltime += gameTime.ElapsedGameTime.Milliseconds;
         }
 
         public override void Draw(SpriteBatch spriteBatch)
         {
-            spriteBatch.Draw(texture, position - GameState.player.position + Player.PLAYERDRAWPOS, null, Color.White, 0f,
+            spriteBatch.Draw(anim[(totaltime / ANIMTIME) % 4], position - GameState.player.position + Player.PLAYERDRAWPOS, null, Color.White, 0f,
                     new Vector2(texture.Width / 2, texture.Height / 2), 1f, SpriteEffects.None, 0.9f);
         }
 
